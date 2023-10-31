7a68444633af6e0226cdeffaa4ae4ea05d0298fe
ProtossDragoon <dlwkdgn1@naver.com>
2023-10-31 14:09:30 +0900

    lesson2: 조금 더 동적인 CMake - CMake와 헤더파일 구성의 소소한 팁

--- modules/module1/include/module1/ClassMat.hpp
+++ modules/module1/include/module1/ClassMat.hpp
@@ -1,5 +1,7 @@
-#ifndef HELLO_CMAKE_CLASSMAT_HPP
-#define HELLO_CMAKE_CLASSMAT_HPP
+// '헤더 가드'는 C++ 에서 유일하게 메크로가 허용되는 경우임
+// 꿀팁: 언더스코어 두 개 '__'로 계층 구조를 관리하면 좋음
+#ifndef MODULE_1__CLASSMAT_HPP
+#define MODULE_1__CLASSMAT_HPP
 
 #include "opencv2/opencv.hpp"
 
